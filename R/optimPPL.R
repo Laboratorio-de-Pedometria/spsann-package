67c23a410b48d5acf1ed858fcd32b92fe1482622
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-01-07 15:23:41 -0200

    Package documentation

--- R/optimPPL.R
+++ R/optimPPL.R
@@ -2,18 +2,17 @@
 #' 
 #' Optimize a sample pattern for variogram estimation using spatial
 #' simulated annealing. The criterion used is the number of
-#' points (or point-pairs) per lag distance class (PPL).
+#' points or point-pairs per lag distance class (\code{optimPPL}). 
+#' \code{pointsPerLag} and \code{pairsPerLag} count the number of points or 
+#' point-pairs per lag distance class. \code{objPoints} and \code{objPairs} 
+#' compute the deviation of the observed distribution of counts from a 
+#' pre-specified distribution, or the minimum number of points or point pairs 
+#' observed over all lag distance classes.
 #' 
-#' Functions to counts the number of points or 
-#' point pairs per lag distance class. Functions to compute the deviation of 
-#' the observed distribution of counts from a pre-specified distribution. 
-#' Functions to compute the minimum number of points or point pairs observed 
-#' over all lag distance classes.
-#' 
-#' @param lags Integer value defining the number of lag distance classes. 
-#' Alternatively, a vector of numeric values defining the lower and upper limits 
-#' of each lag distance class. The lowest value should be different from zero, 
-#' e.g. 0.0001. Defaults to \code{lags = 7}.
+#' @param lags Integer. The number of lag distance classes. Alternatively, a 
+#' vector of numeric values with the lower and upper limits of each lag 
+#' distance class. The lowest value must be different from zero, e.g. 0.0001. 
+#' Defaults to \code{lags = 7}.
 #' 
 #' @param lags.type Character value defining the type of lag distance classes. 
 #' Available options are \code{"equidistant"}, for equidistant lag distance 
@@ -51,130 +50,49 @@
 #' \subsection{Distances}{
 #' Euclidean distances between points are calculated. This computation requires
 #' the coordinates to be projected. The user is responsible for making sure that
-#'  this requirement is attained.
+#' this requirement is attained.
 #' }
 #' \subsection{Distribution}{
-#' Using the default uniform distribution means that the number of 
+#' Using the default uniform distribution means that the desired number of 
 #' \strong{point-pairs} per lag distance class is equal to 
-#' \eqn{n \times (n - 1) / (2 \times lag)}, where \eqn{n} is the total number 
-#' of points in \code{points}, and \eqn{lag} is the number of lag distance 
-#' classes.
+#' \eqn{n \times (n - 1) / (2 \times lag)}, where \eqn{n} is the total number
+#' of points and \eqn{lag} is the number of lag distance classes.
 #' 
-#' Using the default uniform distribution means that the number of 
-#' \strong{points} per lag distance class is equal to the total number of points
-#' in \code{points}. This is the same as expecting that each point contributes 
+#' Using the default uniform distribution means that the desired number of 
+#' \strong{points} per lag distance class is equal to the total number of 
+#' points. This is the same as expecting that each point contributes 
 #' to every lag distance class.
 #' 
-#' Distributions other that the available options can be easily implemented 
+#' Distributions other than the available options can be easily implemented 
 #' changing the arguments \code{lags}, \code{lags.base} and \code{pre.distri}.
 #' }
 #' \subsection{Type of lags}{
 #' Two types of lag distance classes can be created by default. The first 
-#' (\code{lags.type = "equidistant"}) are evenly spaced lags. They are created 
+#' are evenly spaced lags (\code{lags.type = "equidistant"}). They are created 
 #' by simply dividing the distance interval from 0.0001 to \code{cutoff} by the
 #' required number of lags. The minimum value of 0.0001 guarantees that a point
 #' does not form a pair with itself.
 #' 
-#' The second type (\code{lags.type = "exponential"}) of lag distance classes 
-#' is defined by exponential spacings. The spacings are defined by the base 
+#' The second type of lag distance classes is defined by exponential spacings
+#' (\code{lags.type = "exponential"}). The spacings are defined by the base 
 #' \eqn{b} of the exponential expression \eqn{b^n}, where \eqn{n} is the 
-#' required number of lags. The base is defined using argument \code{lags.base}.
-#' For example, the default \code{lags.base = 2} creates lags that are 
-#' sequentially defined as half of the immediately preceding larger lag. If 
-#' \code{cutoff = 100} and \code{lags = 4}, the upper limits of the lag distance
-#' classes will be
-#'  
-#' \verb{
-#' > 100 / (2 ^ c(1:4))
-#' [1] 50.00 25.00 12.50  6.25
-#' }
+#' required number of lags. The base is defined using the argument 
+#' \code{lags.base}.
 #' }
 #' \subsection{Criteria}{
-#' The functions \code{objPoints} and \code{objPairs} (to be implemented) and 
-#' were designed to be used in spatial simulated annealing to optimize spatial 
-#' sample configurations. Both of them have two criteria implemented. The first
-#' is called using \code{criterion = "distribution"} and is used to minimize the
-#' sum of differences between a pre-specified distribution and the observed 
+#' There are two optimizing criteria implemented. The first is called using
+#' \code{criterion = "distribution"} and is used to minimize the sum of
+#' differences between a pre-specified distribution and the observed 
 #' distribution of points or point-pairs per lag distance class.
-#' 
-#' Consider that we aim at having the following distribution of points per lag: 
-#' 
-#' \code{desired <- c(10, 10, 10, 10, 10)},
-#' 
-#' and that the observed distribution of points per lag is the following:
-#' 
-#' \code{observed <- c(1, 2, 5, 10, 10)}.
-#' 
-#' The objective at each iteration of the optimization will be to match the two 
-#' distributions. This criterion is of the same type as the one proposed by 
-#' Warrick and Myers (1987).
 #' 
 #' The second criterion is called using \code{criterion = "minimum"}. It 
 #' corresponds to maximizing the minimum number of points or point-pairs 
-#' observed over all lag distance classes. Consider we observe the following 
-#' distribution of points per lag in the first iteration:
-#' 
-#' \code{observed <- c(1, 2, 5, 10, 10)}.
-#' 
-#' The objective in the next iteration will be to increase the number of points
-#' in the first lag (\eqn{n = 1}). Consider we then have the following resulting
-#' distribution:
-#' 
-#' \code{resulting <- c(5, 2, 5, 10, 10)}.
-#' 
-#' Now the objective will be to increse the number of points in the second lag
-#' (\eqn{n = 2}). The optimization continues until it is not possible to 
-#' increase the number of points in any of the lags, that is, when:
-#' 
-#' \code{distribution <- c(10, 10, 10, 10, 10)}.
-#' 
-#' This shows that the result of using \code{criterion = "minimum"} is similar
-#' to using \code{criterion = "distribution"}. However, the resulting sample 
-#' pattern can be significantly different. The running time of each iteration 
-#' can be a bit longer when using \code{criterion = "distribution"}, but since 
-#' it is a more sensitive criteria (it takes all lags into account), convergence
-#' is likely to be attained with a smaller number of iterations. Note that this
-#' also depends on the other parameters passed to the optimization algorithm.
-#' 
-#' It is important to note that using the first criterion 
-#' (\code{"distribution"}) in simulated annealing corresponds to a 
-#' \strong{minimization} problem. On the other hand, using the second criterion
-#' (\code{"minimum"}) would correspond to a \strong{maximization} problem. We 
-#' solve this inconsistency substituting the criterion that has to be maximized
-#' by its inverse. For conveninence we multiply the resulting value by a 
-#' constant (i.e. \eqn{c / x + 1}, where \code{c} is the number of points and 
-#' \code{x} is the criterion value). This procedure allows us to define both
-#' problems as minimization problems.
+#' observed over all lag distance classes.
 #' }
-#' \subsection{Utopia and nadir points}{
-#' Knowledge of the utopia and nadir points can help in the construction of 
-#' multi-objective optimization problems.
-#' 
-#' When \code{criterion = "distribution"}, the \strong{utopia} 
-#' (\eqn{f^{\circ}_{i}}) point is exactly zero (\eqn{f^{\circ}_{i} = 0}). When 
-#' \code{criterion = "minimum"}, the utopia point is approximately 1 (0.9)
-#' (\eqn{f^{\circ}_{i} \sim 1}). It can be calculated using the equation
-#' \eqn{n / n + 1}, where \code{n} is the number of points 
-#' (\code{objPoints}), or the number point-pairs divided by the number of lag 
-#' distance classes (\code{objPairs}).
-#' 
-#' THIS SECTION ABOUT THE NADIR POINT HAS TO BE REVIEWED!
-#' The \strong{nadir} (\eqn{f^{max}_{i}}) point depends on a series of 
-#' elements. For instance, when \code{criterion = "distribution"}, if the 
-#' desired distribution of point or point-pairs per lag distance class is 
-#' \code{pre.distribution <- c(10, 10, 10, 10, 10)}, the worst case scenario 
-#' would be to have all points or point-pairs in a single lag distance class, 
-#' that is, \code{obs.distribution <- c(0, 50, 0, 0, 0)}. In this case, the 
-#' nadir point is equal to the sum of the differences between the two 
-#' distributions:
-#' 
-#' \code{sum((c(10, 10, 10, 10, 10) - c(0, 50, 0, 0, 0)) ^ 2) = 2000}.
-#' 
-#' When \code{objective = "minimum"}, the nadir point is equal to 
-#' \eqn{f^{max}_{i} = n / 0 + 1 = n}.
-#' }
-#' 
 #' @return
+#' \code{optimPPL} returns a data.frame with the coordinates of the optimized
+#' sample pattern.
+#' 
 #' \code{pointsPerLag} and \code{pairsPerLag} return a data.frame with three 
 #' columns: a) the lower and b) upper limits of each lag distance class, and 
 #' c) the number of points or point-pairs per lag distance class.
@@ -207,21 +125,6 @@
 #' 
 #' @author
 #' Alessandro Samuel-Rosa \email{alessandrosamuelrosa@@gmail.com}
-#' @note
-#' Use \code{lags = 1} with \code{pointsPerLag} and \code{pairsPerLag} to check
-#' that the functions are working correctly. They should return the total number 
-#' of points in \code{points} and the total possible number of point-pairs 
-#' \eqn{n \times (n - 1) / 2}, respectively.
-#' 
-#' Some of the solutions used to build this function were found in the source 
-#' code of the R-packages \strong{intamapInteractive} and \pkg{SpatialTools}.
-#' As such, the authors of those packages (Edzer Pebesma 
-#' <\email{edzer.pebesma@@uni-muenster.de}>, Jon Skoien 
-#' <\email{jon.skoien@@gmail.com}>, Joshua French 
-#' <\email{joshua.french@@ucdenver.edu}>) are entitled \sQuote{contributors} to
-#' the R-package \pkg{pedometrics}.
-#' 
-#' @seealso \code{\link[stats]{dist}}.
 #' @aliases optimPPL pointsPerLag objPoints pairsPerLag objPairs
 #' @keywords spatial optimize
 #' @concept simulated annealing
@@ -233,7 +136,11 @@
 #' meuse <- matrix(cbind(c(1:dim(meuse)[1]), meuse), ncol = 3)
 #' pointsPerLag(meuse, cutoff = 1000)
 #' objPoints(meuse, cutoff = 1000)
-#' 
+# UNIT TEST ####################################################################
+# Use \code{lags = 1} with \code{pointsPerLag} and \code{pairsPerLag} to check
+# that the functions are working correctly. They should return the total number 
+# of points in \code{points} and the total possible number of point-pairs 
+# \eqn{n \times (n - 1) / 2}, respectively.
 # FUNCTION - MAIN ##############################################################
 optimPPL <-
   function (points, candidates, lags = 7, lags.type = "exponential", 
@@ -242,8 +149,10 @@ optimPPL <-
             acceptance = list(initial = 0.99, cooling = iterations / 10),
             stopping = list(max.count = iterations / 10), plotit = TRUE,
             boundary, progress = TRUE, verbose = TRUE) {
+    
+    # ASR: check lags... can be a vector with lower and upper limits
     if (ncol(candidates) != 3) stop ("'candidates' must have three columns")
-    if (plotit) par0 <- par()
+    if (plotit) par0 <- par() # ASR: put on.exit()
     if (is.integer(points)) {
       n_pts <- points
       points <- sample(c(1:dim(candidates)[1]), n_pts)
