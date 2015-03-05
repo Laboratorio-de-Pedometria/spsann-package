833a9ecbc54ab4d56572eeb72f912dbb1851190a
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-03-05 10:33:39 -0300

    Updated default example and tests

--- battlefield/optimDIST_battle.R
+++ battlefield/optimDIST_battle.R
@@ -1,10 +1,10 @@
 # Initial settings
-rm(list = ls())
-gc()
 require(ASRtools)
 require(pedometrics)
 require(sp)
 require(rgeos)
+rm(list = ls())
+gc()
 source('R/optimDIST.R')
 source('R/optimACDC.R')
 source('R/spSANNtools.R')
@@ -36,16 +36,12 @@ res <- optimDIST(points = points, candi = candi, covars = covars,
                  y.max = y.max, y.min = y.min, boundary = boundary, 
                  iterations = iterations)
 tail(attr(res, "energy"), 1) # 0.9897776
-objDIST(points = res, candi = candi, covars = covars, use.coords = TRUE)
+objDIST(points = res, candi = candi, covars = covars, use.coords = use.coords)
 # 1) FACTOR COVARIATES WITH THE COORDINATES ####################################
 rm(list = ls())
 gc()
-source("/home/alessandro/PROJECTS/r-packages/pedometrics/R/numint.R")
-require(ASRtools)
-require(pedometrics)
-require(sp)
-require(rgeos)
 source('R/optimDIST.R')
+source('R/optimACDC.R')
 source('R/spSANNtools.R')
 source('R/spJitter.R')
 Rcpp::sourceCpp('src/spJitterCpp.cpp')
@@ -64,26 +60,24 @@ x.min <- 40
 strata.type <- "area"
 covars <- meuse.grid[, 6:7]
 use.coords <- TRUE
+points <- 100
+iterations <- 1000
 set.seed(2001)
-res <- optimDIST(points = 100, candi = candi, covars = covars, 
-                 strata.type = "area", use.coords = use.coords, x.max = x.max, 
-                 x.min = x.min, y.max = y.max, y.min = y.min, 
-                 boundary = boundary, iterations = 100)
-tail(attr(res, "energy"), 1) # 125.0564
+res <- optimDIST(points = points, candi = candi, covars = covars, 
+                 strata.type = strata.type, use.coords = use.coords, 
+                 x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min, 
+                 boundary = boundary, iterations = iterations)
+tail(attr(res, "energy"), 1) # 0.6863938
 objDIST(points = res, candi = candi, covars = covars, use.coords = use.coords,
         strata.type = strata.type)
 # 2) FACTOR AND NUMERIC COVARIATES WITH THE COORDINATES ########################
 rm(list = ls())
 gc()
-require(ASRtools)
-require(pedometrics)
-require(sp)
-require(rgeos)
 source('R/optimDIST.R')
+source('R/optimACDC.R')
 source('R/spSANNtools.R')
 source('R/spJitter.R')
 Rcpp::sourceCpp('src/spJitterCpp.cpp')
-source("/home/alessandro/PROJECTS/r-packages/pedometrics/R/numint.R")
 data(meuse.grid)
 candi <- meuse.grid[, 1:2]
 coordinates(candi) <- ~ x + y
@@ -97,13 +91,16 @@ y.max <- diff(bbox(boundary)[2, ])
 y.min <- 40
 x.min <- 40
 points <- 100
-iterations <- 100
+iterations <- 1000
 strata.type <- "area"
 covars <- meuse.grid[, c(1, 2, 5:7)]
 str(covars)
 use.coords <- TRUE
 set.seed(2001)
-res <- optimDIST(points = points, candi = candi, covars = covars, strata.type = strata.type, use.coords = use.coords, x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min, boundary = boundary, iterations = iterations)
-tail(attr(res, "energy"), 1) # 305.554
+res <- optimDIST(points = points, candi = candi, covars = covars, 
+                 strata.type = strata.type, use.coords = use.coords, 
+                 x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min,
+                 boundary = boundary, iterations = iterations)
+tail(attr(res, "energy"), 1) # 1.870686
 objDIST(points = res, candi = candi, covars = covars, use.coords = use.coords, 
         strata.type = strata.type)
