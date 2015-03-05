8f9207ad6406c5faa32236b6c144be24822e5e8e
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-03-05 17:45:39 -0300

    New test

--- battlefield/optimCORR_battle.R
+++ battlefield/optimCORR_battle.R
@@ -69,3 +69,35 @@ res <- optimCORR(points = points, candi = candi, covars = covars,
                  boundary = boundary, iterations = iterations)
 tail(attr(res, "energy"), 1) # 1.249576
 objCORR(points = res, candi = candi, covars = covars, use.coords = use.coords)
+# 2) FACTOR COVARIATES #########################################################
+rm(list = ls())
+gc()
+source('R/optimCORR.R')
+source('R/optimACDC.R')
+source('R/spSANNtools.R')
+source('R/spJitter.R')
+Rcpp::sourceCpp('src/spJitterCpp.cpp')
+data(meuse.grid)
+candi <- meuse.grid[, 1:2]
+coordinates(candi) <- ~ x + y
+gridded(candi) <- TRUE
+boundary <- as(candi, "SpatialPolygons")
+boundary <- gUnionCascaded(boundary)
+candi <- coordinates(candi)
+candi <- matrix(cbind(1:dim(candi)[1], candi), ncol = 3)
+str(meuse.grid)
+covars <- meuse.grid[, rep(6:7, 3)]
+x.max <- diff(bbox(boundary)[1, ])
+y.max <- diff(bbox(boundary)[2, ])
+y.min <- 40
+x.min <- 40
+use.coords <- FALSE
+iterations <- 1000
+points <- 100
+set.seed(2001)
+res <- optimCORR(points = points, candi = candi, covars = covars, 
+                 use.coords = use.coords, x.max = x.max, x.min = x.min, 
+                 y.max = y.max, y.min = y.min, 
+                 boundary = boundary, iterations = iterations)
+tail(attr(res, "energy"), 1) # 0.01085086
+objCORR(points = res, candi = candi, covars = covars, use.coords = use.coords)
