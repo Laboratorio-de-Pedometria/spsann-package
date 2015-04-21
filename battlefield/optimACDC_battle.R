83b6275f94578e8f7f4e97e1f3b137356a785245
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-04-21 16:27:54 +0200

    New utopia definitions

--- battlefield/optimACDC_battle.R
+++ battlefield/optimACDC_battle.R
@@ -25,20 +25,20 @@ covars <- meuse.grid[, 5]
 x.max <- diff(bbox(boundary)[1, ])
 y.max <- diff(bbox(boundary)[2, ])
 nadir <- list(sim = 10, save.sim = TRUE, user = NULL, abs = NULL)
-utopia <- list(user = list(correl = 0, strata = 0), abs = NULL)
+utopia <- list(user = list(DIST = 0, CORR = 0), abs = NULL)
 scale <- list(type = "upper-lower", max = 100)
-weights <- list(strata = 0.5, correl = 0.5)
+weights <- list(DIST = 0.5, CORR = 0.5)
 set.seed(2001)
 res <- optimACDC(points = 100, candi = candi, covars = covars,
                  use.coords = TRUE, weights = weights, x.max = x.max, 
                  x.min = 40, y.max = y.max, y.min = 40,
                  boundary = boundary, nadir = nadir, iterations = 1000,
                  utopia = utopia, scale = scale)
-tail(attr(res, "energy"), 1) # 55.59217
+tail(attr(res, "energy"), 1) # 0.6072596
 objACDC(points = res, candi = candi, covars = covars, use.coords = TRUE, 
         weights = weights, nadir = nadir, utopia = utopia, scale = scale)
 
-# 1) FACTOR COVARIATES USING THE COORDINATES ###################################
+# 1) FACTOR COVARIATES USING THE COORDINATES, WITH USER-DEFINED NADIR ##########
 rm(list = ls())
 gc()
 require(ASRtools)
@@ -58,29 +58,20 @@ boundary <- gUnionCascaded(boundary)
 candi <- coordinates(candi)
 candi <- matrix(cbind(1:dim(candi)[1], candi), ncol = 3)
 covars <- meuse.grid[, 6:7]
-covars.type <- "factor"
-points <- 100
 x.max <- diff(bbox(boundary)[1, ])
-y.min <- x.min <- 40
 y.max <- diff(bbox(boundary)[2, ])
-iterations <- 100
-acceptance <- list(initial = 0.99, cooling = iterations / 10)
-weights <- list(strata = 0.5, correl = 0.5)
-use.coords <- TRUE
-nadir <- list(sim = 10, save.sim = TRUE, user = NULL, abs = NULL)
-utopia <- list(user = list(correl = 0, strata = 0), abs = NULL)
+nadir <- list(sim = NULL, save.sim = NULL, user = list(DIST = 10, CORR = 1), abs = NULL)
+utopia <- list(user = list(DIST = 0, CORR = 0), abs = NULL)
 scale <- list(type = "upper-lower", max = 100)
 set.seed(2001)
-tmp <- optimACDC(points = points, candi = candi, covars = covars, 
-                 covars.type = covars.type, use.coords = use.coords, 
-                 x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min,
-                 boundary = boundary, iterations = iterations, 
-                 weights = weights, acceptance = acceptance, nadir = nadir,
-                 utopia = utopia, scale = scale)
-tail(attr(tmp, "energy"), 1) # 60.25659
-objACDC(points = tmp, candi = candi, covars = covars, use.coords = use.coords, 
-        covars.type = covars.type, weights = weights, nadir = nadir,
-        utopia = utopia, scale = scale) # 60.25659
+tmp <- optimACDC(points = 100, candi = candi, covars = covars, 
+                 use.coords = TRUE, x.max = x.max, x.min = 40, y.max = y.max,
+                 y.min = 40, boundary = boundary, iterations = 100, 
+                 nadir = nadir, utopia = utopia, scale = scale)
+tail(attr(tmp, "energy"), 1) # 2.23372
+objACDC(points = tmp, candi = candi, covars = covars, use.coords = TRUE, 
+        nadir = nadir, utopia = utopia, scale = scale)
+
 # 3) FACTOR COVARIATES USING THE COORDINATES WITH A FEW POINTS #################
 # The following error appeared in an old version (before correcting for the 
 # number of strata) when the number of points is small (n = 5, seed = 2001):
@@ -111,50 +102,52 @@ boundary <- gUnionCascaded(boundary)
 candi <- coordinates(candi)
 candi <- matrix(cbind(1:dim(candi)[1], candi), ncol = 3)
 covars <- meuse.grid[, 6:7]
-covars.type <- "factor"
-use.coords <- TRUE
 x.max <- diff(bbox(boundary)[1, ])
-y.min <- x.min <- 40
 y.max <- diff(bbox(boundary)[2, ])
-iterations <- 100
-acceptance <- list(initial = 0.99, cooling = iterations / 10)
-weights <- list(strata = 0.5, correl = 0.5)
 nadir <- list(sim = 10, save.sim = TRUE, user = NULL, abs = NULL)
 utopia <- list(user = list(correl = 0, strata = 0), abs = NULL)
 scale <- list(type = "upper-lower", max = 100)
-points <- 10
 set.seed(2001)
-tmp <- optimACDC(points = points, candi = candi, covars = covars,
-                 covars.type = covars.type, use.coords = use.coords,
-                 x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min,
-                 boundary = boundary, iterations = iterations,
-                 weights = weights, acceptance = acceptance, nadir = nadir,
+tmp <- optimACDC(points = 10, candi = candi, covars = covars, use.coords = TRUE,
+                 x.max = x.max, x.min = 40, y.max = y.max, y.min = 40,
+                 boundary = boundary, iterations = 100, nadir = nadir,
                  utopia = utopia, scale = scale)
-objACDC(points = points, candi = candi, covars = covars,
-        covars.type = covars.type, use.coords = use.coords, weights = weights,
+tail(attr(tmp, "energy"), 1) # 2.084277
+objACDC(points = tmp, candi = candi, covars = covars, use.coords = TRUE, 
         nadir = nadir, utopia = utopia, scale = scale)
+
 # 4) CATEGORICAL COVARIATES WITH MANY COVARIATES AND MANY POINTS ###############
 # The function table() in the functions cramer() and chisqTest() is the one
 # taking most of the time to run. Can we implement it in C++?
+rm(list = ls())
+gc()
+require(ASRtools)
+require(pedometrics)
+require(sp)
+require(rgeos)
+source('R/optimACDC.R')
+source('R/spSANNtools.R')
+source('R/spJitter.R')
+Rcpp::sourceCpp('src/spJitterCpp.cpp')
+data(meuse.grid)
+candi <- meuse.grid[, 1:2]
+coordinates(candi) <- ~ x + y
+gridded(candi) <- TRUE
+boundary <- as(candi, "SpatialPolygons")
+boundary <- gUnionCascaded(boundary)
+candi <- coordinates(candi)
+candi <- matrix(cbind(1:dim(candi)[1], candi), ncol = 3)
 covars <- meuse.grid[, rep(c(6, 7), 10)]
-points <- 500
 x.max <- diff(bbox(boundary)[1, ])
-y.min <- x.min <- 40
 y.max <- diff(bbox(boundary)[2, ])
-iterations <- 2
-acceptance <- list(initial = 0.99, cooling = iterations / 10)
-weights <- list(strata = 0.5, correl = 0.5)
-continuous <- FALSE
-use.coords <- FALSE
-stopping <- list(max.count = iterations / 10)
-sim.nadir <- 1
-strata.type <- "equal.area"
-plotit <- TRUE
-progress <- TRUE
-verbose <- FALSE
+nadir <- list(sim = 10, save.sim = TRUE, user = NULL, abs = NULL)
+utopia <- list(user = list(correl = 0, strata = 0), abs = NULL)
+scale <- list(type = "upper-lower", max = 100)
 set.seed(2001)
-Rprof(a <- tempfile())
-tmp <- optimACDC(points = points, candidates = candidates, covars = covars, continuous = continuous, use.coords = use.coords, x.max = x.max, x.min = x.min, y.max = y.max, y.min = y.min, boundary = boundary, iterations = iterations, weights = weights, acceptance = acceptance, sim.nadir = sim.nadir, stopping = stopping, plotit = plotit, progress = progress, verbose = verbose, strata.type = strata.type)
-Rprof()
-summaryRprof(a)
-unlink(a)
+tmp <- optimACDC(points = 500, candi = candi, covars = covars, 
+                 use.coords = TRUE, x.max = x.max, x.min = 40, y.max = y.max, 
+                 y.min = 40, boundary = boundary, iterations = 100, 
+                 nadir = nadir, utopia = utopia, scale = scale)
+tail(attr(tmp, "energy"), 1) # 4.75408
+objACDC(points = tmp, candi = candi, covars = covars, use.coords = TRUE, 
+        nadir = nadir, utopia = utopia, scale = scale)
--- man-roxygen/MOOP_doc.R
+++ man-roxygen/MOOP_doc.R
@@ -1,5 +1,19 @@
 #  Template documentation for multi-objective optimization problems (MOOP)
 ################################################################################
+#' @param nadir List with four named sub-arguments: \code{sim} -- the number of
+#' random realizations to estimate the nadir point; \code{save.sim} -- logical 
+#' for saving the simulated values and returning them as an attribute of the 
+#' optimized sample configuration; \code{user} -- a list of user-defined 
+#' values named after the objective function to which they apply; 
+#' \code{abs} -- logical for calculating the nadir point internally. 
+#' Defaults to \code{sim = 1000}, \code{save.sim = TRUE}, \code{user = NULL}, 
+#' and \code{abs = NULL}.
+#'
+#' @param utopia List with two named sub-arguments: \code{user} -- a list of
+#' user-defined values named after the objective function to which they apply, 
+#' and \code{abs} -- logical for calculating the utopia point internally. 
+#' Defaults to \code{user = NULL} and \code{abs = NULL}.
+#'
 #' @references
 #' Arora, J. \emph{Introduction to optimum design}. Waltham: Academic Press, p. 
 #' 896, 2011.
