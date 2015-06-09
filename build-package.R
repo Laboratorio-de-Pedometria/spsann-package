41f2c058abf3055012bde3aa0e97bff31cf0e678
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-06-09 11:30:34 -0300

    use package devtools

---
+++ build-package.R
@@ -0,0 +1,15 @@
+# Build package
+
+# turn on/off development mode
+dev_mode()
+
+# check examples and documentation
+run_examples()
+check_doc()
+
+# check the package for Linux and Windows
+check()
+build_win()
+
+# upload to CRAN
+release()
