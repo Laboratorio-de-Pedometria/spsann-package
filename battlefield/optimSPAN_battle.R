5e16db336f2e964441f4732ab3a6717b9acba5b6
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-07-03 19:59:00 -0300

    default example

--- battlefield/optimSPAN_battle.R
+++ battlefield/optimSPAN_battle.R
@@ -1,48 +1,24 @@
 # Initial settings
 rm(list = ls())
 gc()
-require(ASRtools)
 require(pedometrics)
-require(sp)
-require(rgeos)
-require(Hmisc)
+require(SpatialTools)
 sapply(list.files("src", full.names = TRUE, pattern = ".cpp$"), Rcpp::sourceCpp)
 sapply(list.files("R", full.names = TRUE, pattern = ".R$"), source)
 # PREPARE DATA #################################################################
 require(sp)
 data(meuse.grid)
-candidates <- meuse.grid[, 1:2]
-coordinates(candidates) <- ~ x + y
-gridded(candidates) <- TRUE
-boundary <- as(candidates, "SpatialPolygons")
-boundary <- gUnionCascaded(boundary)
-candidates <- coordinates(candidates)
-candidates <- matrix(cbind(c(1:dim(candidates)[1]), candidates), ncol = 3)
-points <- 100
-x.max <- diff(bbox(boundary)[1, ])
-y.min <- x.min <- 40
-y.max <- diff(bbox(boundary)[2, ])
-iterations <- 10000
-acceptance <- list(initial = 0.99, cooling = iterations / 10)
-weights <- list(strata = 0.5, correl = 0.5)
-stopping <- list(max.count = iterations / 10)
-plotit <- TRUE
-progress <- TRUE
-verbose <- TRUE
-PPL <- list(lags.type  = "exponential",
-            criterion  = "distribution",
-            lags       = 7,
-            lags.base  = 2,
-            pre.distri = NULL,
-            cutoff     = sqrt((x.max * x.max) + (y.max * y.max)))
-ACDC <- list(covars      = meuse.grid[, 1],
-             covars.type = "numeric",
-             use.coords  = TRUE,
-             strata.type = "equal.area",
-             weights     = list(strata = 0.5, correl = 0.5))
-PAN <- list(weights = list(PPL = 1/3, ACDC = 1/3, MSSD = 1/3),
-            nadir   = list(sim = 10, save.sim = FALSE, user = NULL, 
-                           abs = NULL))
+candi <- meuse.grid[, 1:2]
+nadir <- list(sim = 10, seeds = 1:10)
+utopia <- list(user = list(DIST = 0, CORR = 0, PPL = 0, MSSD = 0))
+covars <- meuse.grid[, 5]
+set.seed(2001)
+res <- optimSPAN(points = 100, candi = candi, covars = covars, nadir = nadir,
+                 use.coords = TRUE, iterations = 1000, utopia = utopia, 
+                 verbose = FALSE, plotit = FALSE, track = FALSE)
+tail(attr(res, "energy")$obj, 1) # 
+str(res)
+
 # 1) FIRST TEST ################################################################
 x11()
 set.seed(2001)
