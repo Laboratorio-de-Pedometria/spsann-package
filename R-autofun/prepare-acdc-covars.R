0d4b00dbd9ed6d72039e27a1ee82547b1dc77c76
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-07-14 15:04:39 -0300

    compute breaks

--- R-autofun/prepare-acdc-covars.R
+++ R-autofun/prepare-acdc-covars.R
@@ -1,23 +1,22 @@
 # Prepare ACDC covariates
 #
 # SUMMARY
-# 1. Identify the type of covariates (factor or numeric);
-# 2. Prepare the object 'covars';
-# 2.1 Check if the coordinates should be used;
-# 2.2 Convert numeric covariates to factor covariates if needed;
-# 3. Count the number of covariates;
-# 4. Prepare the starting sample matrix.
+# 1. Check if the coordinates should be used;
+# 2. Identify the type of covariates (factor or numeric);
+# 3. If needed, convert numeric covariates to factor covariates;
+# 4. Count the number of covariates;
+# 5. Prepare the starting sample matrix.
 #
 # NOTES
 # 1. 
-
-# Factor or numeric?
-covars.type <- ifelse(pedometrics::is.any.factor(covars), "factor", "numeric")
 
 # Use coordinates?
 if (use.coords) {
   covars <- data.frame(covars, candi[, 2:3])
 }
+
+# Factor or numeric?
+covars.type <- ifelse(pedometrics::is.any.factor(covars), "factor", "numeric")
 
 # Convert numeric covariates to factor covariates
 if (covars.type == "factor") {
@@ -28,7 +27,7 @@ if (covars.type == "factor") {
     message(mes)
     num_covars <- data.frame(covars[, i])
     breaks <- .strataACDC(n.pts = n_pts, strata.type = strata.type, 
-                          covars = num_covars, covars.type = covars.type)[[1]]
+                          covars = num_covars, covars.type = "numeric")[[1]]
     num_covars <- pedometrics::cont2cat(x = num_covars, breaks = breaks)
     covars[, i] <- num_covars
   }
