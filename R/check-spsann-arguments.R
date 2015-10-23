fc56a6d7b61de7c97bf511c667a796504ba973ac
samuel-rosa <alessandrosamuelrosa@gmail.com>
2015-10-23 17:42:56 -0200

    track if plotit = TRUE

--- R/check-spsann-arguments.R
+++ R/check-spsann-arguments.R
@@ -80,6 +80,7 @@ expression(res <- NULL, aa <- c("points", "candi"), bb <- c(missing(points), mis
         }  
       }
     }, if (track || plotit) {
+      if (plotit) { track <- TRUE }
       accept_probs <- vector()
       if (MOOP) {
         energies <- as.data.frame(matrix(NA, nrow = 1, ncol = length(weights) + 1))
