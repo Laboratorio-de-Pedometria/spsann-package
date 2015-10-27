54f8147ec0ab18d48d60e35a79af24cb62c8557e
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-10-27 19:49:51 -0200

    devel version

--- R/spsann.R
+++ R/spsann.R
@@ -30,8 +30,8 @@
 #' \tabular{ll}{
 #' Package: \tab spsann    \cr
 #' Type:    \tab Package   \cr
-#' Version: \tab 1.0-2     \cr
-#' Date:    \tab 2015-10-25\cr
+#' Version: \tab 1.0-2.9000\cr
+#' Date:    \tab 2015-10-27\cr
 #' License: \tab GPL (>= 2)\cr
 #' }
 #' 
