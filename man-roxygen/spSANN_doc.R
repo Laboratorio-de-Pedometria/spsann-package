18ca461d59e408af5a07a75a0f9350d74eed61ff
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-11-10 15:45:03 -0200

    improved documentation

--- man-roxygen/spSANN_doc.R
+++ man-roxygen/spSANN_doc.R
@@ -3,14 +3,14 @@
 #' @param schedule List with 10 named sub-arguments defining the control
 #' parameters of the cooling schedule. See \code{scheduleSPSANN}.
 #' 
-#' @param plotit Logical for plotting the optimization results. This includes 
-#' a) the progress of the objective function values and acceptance 
-#' probabilities, and b) the original points, the perturbed points and the 
-#' progress of the maximum perturbation in the x- and y-coordinates. The plots 
-#' are updated at each 10 iterations. Defaults to \code{plotit = FALSE}.
+#' @param plotit Logical for plotting the optimization results, including
+#' a) the progress of the objective function, and b) the starting (gray) and
+#' current system configuration (black), and the maximum jitter in the 
+#' x- and y-coordinates. The plots are updated at each 10 jitters. Defaults to
+#' \code{plotit = FALSE}.
 #' 
-#' @param boundary SpatialPolygon. The boundary of the spatial domain. 
-#' If missing, it is estimated from \code{candi}.
+#' @param boundary SpatialPolygon defining the boundary of the spatial domain. 
+#' It is estimated from \code{candi} if missing and \code{plotit = TRUE}.
 #' 
 #' @param progress Logical for printing a progress bar. Defaults to 
 #' \code{progress = TRUE}.
@@ -18,10 +18,9 @@
 #' @param verbose Logical for printing messages about the progress of the
 #' optimization. Defaults to \code{verbose = FALSE}.
 #' 
-#' @param track Logical value. Should the evolution of the energy state and 
-#' acceptance probability be recorded and returned with the result? If 
-#' \code{track = FALSE} (the default), only the starting and ending energy state
-#' values are returned with the result.
+#' @param track Logical value. Should the evolution of the energy state be 
+#' recorded and returned with the result? If \code{track = FALSE} (the default),
+#' only the starting and ending energy states are returned with the result.
 #' 
 #' @keywords spatial optimize iteration
 #' 
