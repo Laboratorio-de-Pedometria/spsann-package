e5966055299e43f7e748422ccadefcef8dcb8b53
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-11-10 15:44:23 -0200

    removed description of parameters

--- man-roxygen/spJitter_doc.R
+++ man-roxygen/spJitter_doc.R
@@ -1,67 +1,47 @@
 #  Template documentation for spatial jittering
 ################################################################################
-#' @param points Integer value, integer vector, data frame or matrix. If 
-#' \code{points} is an integer value, it defines the number of points that 
-#' should be randomly sampled from \code{candi} to form the starting system
-#' configuration. If \code{points} is a vector of integer values, it contains 
-#' the row indexes of \code{candi} that correspond to the points that form the
-#' starting system configuration. If \code{points} is a data frame or matrix, 
-#' it must have three columns in the following order: \code{[, "id"]} the 
-#' row indexes of \code{candi} that correspond to each point, \code{[, "x"]} 
-#' the projected x-coordinates, and \code{[, "y"]} the projected y-coordinates.
-#' Note that in the later case, \code{points} must be a subset of \code{candi}.
-#'
-#' @param candi Data frame or matrix with the candidate locations for the
-#' perturbed points. \code{candi} must have two columns in the following 
-#' order: \code{[, "x"]} the projected x-coordinates, and \code{[, "y"]} the 
-#' projected y-coordinates.
-#'
-#' @param x.max,x.min,y.max,y.min Numeric value. The minimum and maximum 
-#' quantity of random noise to be added to the projected x- and y-coordinates.
-#' The minimum quantity should be equal to, at least, the minimum distance
-#' between two neighbouring candidate locations. The units are the same as of 
-#' the projected x- and y-coordinates. If missing, they are estimated from
-#' \code{candi}.
-#'
 #' @section Jittering methods:
 #' There are two ways of jittering the coordinates. They differ on how the
 #' set of candidate locations is defined. The first method uses an 
-#' \emph{infinite} set of candidate locations, that is, any point in the spatial
-#' domain can be selected as the new location of a perturbed point. All that 
-#' this method needs is a polygon indicating the boundary of the spatial domain.
-#' This method is not implemented in the \pkg{spsann} package (yet) because it 
-#' is computationally demanding: every time a point is jittered, it is necessary
-#' to check if it is inside the spatial domain.
+#' \emph{infinite} set of candidate locations, that is, any locations in the 
+#' spatial domain can be selected as the new location of a jittered point. All 
+#' that this method needs is a polygon indicating the boundary of the spatial 
+#' domain. This method is not implemented in the \pkg{spsann}-package because 
+#' it is computationally demanding: every time a point is jittered, it is 
+#' necessary to check if the point is inside the spatial domain.
 #' 
 #' The second method consists of using a \emph{finite} set of candidate 
-#' locations for the perturbed points. A finite set of candidate locations is
+#' locations for the jittered points. A finite set of candidate locations is
 #' created by discretizing the spatial domain, that is, creating a fine grid of
-#' points that serve as candidate locations for the perturbed points. This is 
-#' the only method currently implemented in the \pkg{spsann} package because it
-#' is one of the least computationally demanding.
+#' points that serve as candidate locations for the jittered points. This is
+#' the least computationally demanding jittering method because, by definition, 
+#' the jittered point will always be inside the spatial domain. However, not 
+#' all locations in the spatial domain can be selected as the new location for 
+#' a jittered point.
 #' 
-#' Using a finite set of candidate locations has one important inconvenience.
-#' When a point is selected to be jittered, it may be that the new location 
-#' already is occupied by another point. If this happens, another location is 
-#' iteratively sought for as many times as there are points in \code{points}. 
-#' Because the more points there are in \code{points}, the more likely it is 
-#' that the new location already is occupied by another point. If a solution is
-#' not found, the point selected to be jittered point is kept in its original 
-#' location.
+#' Using a finite set of candidate locations has another important 
+#' inconvenience. When a point is jittered, it may be that the new location 
+#' already is occupied by another point. If this happens, another location has
+#' to be iteratively sought for, say, as many times as the number of points in
+#' the system. In general, the more points there are in the system, the more 
+#' likely it is that the new location already is occupied by another point. If 
+#' a solution is not found in a reasonable time, the point selected to be 
+#' jittered is kept in its original location. Such a proceedure is clearly 
+#' suboptimal.
 #' 
-#' A more elegant method can be defined using a finite set of candidate 
-#' locations coupled with a form of \emph{two-stage random sampling} as 
-#' implemented in \code{\link[spcosa]{spsample}}. Because the candidate 
-#' locations are placed on a finite regular grid, they can be seen as being the 
-#' centre nodes of a finite set of grid cells (or pixels of a raster image). In 
-#' the first stage, one of the \dQuote{grid cells} is selected with replacement,
-#' i.e. independently of already being occupied by another sample point. The new
-#' location for the point chosen to be jittered is selected within that  
-#' \dQuote{grid cell} by simple random sampling. This method guarantees that 
-#' any location in the spatial domain can be a candidate location. It also
-#' discards the need to check if the new location already is occupied by
-#' another point. This method is not implemented (yet) in the \pkg{spsann} 
-#' package.
+#' The \pkg{spsann}-package uses a more elegant method which is based on using 
+#' a finite set of candidate locations coupled with a form of \emph{two-stage 
+#' random sampling} as implemented in \code{\link[spcosa]{spsample}}. Because 
+#' the candidate locations are placed on a finite regular grid, they can be 
+#' seen as being the centre nodes of a finite set of grid cells (or pixels of 
+#' a raster image). In the first stage, one of the \dQuote{grid cells} is 
+#' selected with replacement, i.e. independently of already being occupied by 
+#' another sample point. The new location for the point chosen to be jittered 
+#' is selected within that \dQuote{grid cell} by simple random sampling. This 
+#' method guarantees that \emph{almost} any location in the spatial domain can 
+#' be a candidate location. It also discards the need to check if the new 
+#' location already is occupied by another point, simplifying the code and 
+#' (possibly) speeding up the computations.
 #'
 #' @section Distance between two points:
 #' The distance between two points is computed as the Euclidean distance between
