8b8497d0264c9a57708875acc5ed452c9ae7eeda
samuel-rosa <samuel-rosa@users.noreply.github.com>
2015-11-15 19:59:23 -0200

    prepare output using object of class OSC

--- R-autofun/prepare-output.R
+++ R-autofun/prepare-output.R
@@ -15,24 +15,6 @@
 # Progress bar
 if (progress) close(pb)
 
-# Last system configuration
-res <- new_conf
-a <- attributes(res)
-
-# Energy states
-if (!track) energies <- new_energy
-criterion <- rbind(energy0, energies)
-# if (MOOP) {
-#   criterion <- rbind(energy0, energies)
-# } else {
-#   criterion <- c(energy0, energies)
-# }
-a$energy.state <- criterion
-
-# Other attributes
-a$iterations <- k
-
-# Running time
 rt <- as.numeric(c(proc.time() - time0)[3])
 if (rt > 3600) {
   rt <- list(time = round(rt / 3600, 2), unit = "hours")
@@ -43,22 +25,50 @@ if (rt > 3600) {
     rt <- list(time = round(rt, 2), unit = "seconds")
   }
 }
-a$running.time <- rt
 
-# MOOP
-if (MOOP) {
-  a$weights <- weights
-  if (objective != "CLHS") {
-    a$nadir <- nadir
-    a$utopia <- utopia 
-  }
+# Energy states
+if (!track) energies <- new_energy
+criterion <- rbind(energy0, energies)
+
+res <- new("OptimizedSampleConfiguration", points = data.frame(new_conf))
+slot(res, "spsann") <- 
+  list(acceptance = data.frame(initial = schedule$initial.acceptance),
+       cellsize = data.frame(x = cellsize[1], y = cellsize[2]),
+       chains = data.frame(total = schedule$chains, used = i, 
+                           length = schedule$chain.length),
+       jitter = data.frame(x = c(x.min, x_max0), y = c(y.min, y_max0), 
+                           row.names = c("min", "max")),
+       running = data.frame(time = rt[[1]], units = rt[[2]]),
+       stopping = schedule$stopping,
+       temperature = data.frame(initial = schedule$initial.temperature,
+                                final = actual_temp))
+slot(res, "objective") <-
+  list(name = objective,
+       energy = criterion,
+       nadir = if (MOOP && objective != "CLHS") data.frame(nadir),
+       utopia = if (MOOP && objective != "CLHS") data.frame(utopia),
+       weights = if (MOOP) data.frame(weights))
+
+if (objective %in% c("ACDC", "CLHS", "CORR", "DIST", "SPAN")) {
+  if (objective != "CLHS") res@objective$strata.type <- strata.type
+  res@objective$use.coords <- use.coords
+}
+if (objective %in% c("PPL", "SPAN")) {
+  res@objective$lags <- lags
+  res@objective$lags.type <- lags.type
+  res@objective$lags.base <- lags.base
+  res@objective$cutoff <- cutoff
+  res@objective$criterion <- criterion
+  res@objective$distri <- distri
+  res@objective$pairs <- pairs
+}
+if (objective == "MKV") {
+  res@objective$eqn <- eqn
+  res@objective$vgm <- vgm
+  res@objective$krige.stat <- krige.stat
 }
 
-# Add attributes
-attributes(res) <- a
-
-# Print the number of iterations and running time
-cat("iterations = ", a$iterations, "\n", sep = "")
+# Print the running time
 cat("running time = ", rt$time, " ", rt$unit, sep = "")
 
 # Output
