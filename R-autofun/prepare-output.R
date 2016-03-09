19369997664f7727afd29e96c7091dda94f8b161
samuel-rosa <alessandrosamuelrosa@gmail.com>
2016-03-09 14:43:31 -0300

    internal creation of object of class OSC (not exported anymore)

--- R-autofun/prepare-output.R
+++ R-autofun/prepare-output.R
@@ -31,8 +31,33 @@ if (rt > 3600) {
 if (!track) energies <- new_energy
 energies <- rbind(energy0, energies)
 
-res <- methods::new("OptimizedSampleConfiguration", 
-                    points = data.frame(new_conf))
+# Create object of class 'list' with many slots to store optimization information.
+# Later redefine the class of the object as 'OptimizedSampleConfiguration'
+# res <- methods::new("OptimizedSampleConfiguration", points = data.frame(new_conf))
+res <- list(
+  # The optimized sample configuration
+  points = data.frame(id = NA_integer_, x = NA_real_, y = NA_real_),
+  
+  # Information about the spatial simulated annealing
+  spsann = list(
+    acceptance = data.frame(initial = NA_real_),
+    cellsize = data.frame(x = NA_real_, y = NA_real_),
+    chains = data.frame(total = NA_integer_, used = NA_integer_, length = NA_integer_),
+    jitter = data.frame(x = rep(NA_real_, 2), y = rep(NA_real_, 2), row.names = c("min", "max")),
+    running = data.frame(time = NA_real_, units = NA_character_), 
+    stopping = NA_integer_,
+    temperature = data.frame(initial = NA_real_, final = NA_real_)),
+  
+  # Information about the objective function
+  objective = list(
+    name = NA_character_,
+    energy = data.frame(NA_real_),
+    nadir = data.frame(NA_real_),
+    utopia = data.frame(NA_real_),
+    weights = data.frame(NA_real_))
+)
+class(res) <- "OptimizedSampleConfiguration"
+
 slot(res, "spsann") <- 
   list(acceptance = data.frame(initial = schedule$initial.acceptance),
        cellsize = data.frame(x = cellsize[1], y = cellsize[2]),
