4d5ee4a62e3b27197a0dc16710327403877b9a6e
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-03-25 12:44:15 -0300

    when plotting, account for existing fixed sampling points

--- R/SPSANNtools.R
+++ R/SPSANNtools.R
@@ -76,7 +76,12 @@ objSPSANN.OptimizedSampleConfiguration <-
       sp::plot(x = boundary)
     }
     graphics::points(conf0[, 1], conf0[, 2], pch = 1, cex = 0.5, col = "lightgray")
-    graphics::points(new_conf[, 1], new_conf[, 2], pch = 20, cex = 0.5)
+    # graphics::points(new_conf[, 1], new_conf[, 2], pch = 20, cex = 0.5)
+    pch <- rep(20, n_pts)
+    if (exists("fixed_pts")) {
+      pch <- c(pch, rep(4, n_fixed_pts))
+    }
+    graphics::points(new_conf[, 1], new_conf[, 2], pch = pch, cex = 0.5)
     if (!missing(wp)) {
       graphics::points(new_conf[wp, 1], new_conf[wp, 2], pch = 20, cex = 1, col = "red")
     }
