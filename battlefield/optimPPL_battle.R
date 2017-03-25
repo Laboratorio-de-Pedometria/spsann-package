cc16f539702672ea258aafd29a1a6c1f6db99312
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-03-25 18:42:37 -0300

    added evaluation of sample optimization when we have a set of fixed sample points

--- battlefield/optimPPL_battle.R
+++ battlefield/optimPPL_battle.R
@@ -28,12 +28,11 @@ candi <- meuse.grid[, 1:2]
 schedule <- scheduleSPSANN(initial.temperature = 500)
 set.seed(2001)
 res <- optimPPL(
-  points = 30, candi = candi, pairs = TRUE, schedule = schedule, 
-  plotit = TRUE, boundary = boundary)
+  points = 30, candi = candi, pairs = TRUE, schedule = schedule, plotit = TRUE)
 objSPSANN(res)
 objPPL(points = res, pairs = TRUE, candi = candi)
 countPPL(points = res, candi = candi, pairs = TRUE)
-plot(res, boundary = boundary)
+plot(res)
 
 # 2) Points per lag - select sample points from candi #########################################################
 rm(list = ls())
@@ -67,3 +66,23 @@ set.seed(2001)
 countPPL(points = 100, candi = candi, lags = 1, cutoff = Inf)[3] - 100
 set.seed(2001)
 countPPL(points = 100, candi = candi, lags = 1, pairs = TRUE, cutoff = Inf)[3] - 100 * 99 / 2
+
+# 4) ADD TEN POINTS TO EXISTING SPATIAL SAMPLE ################################################################
+rm(list = ls())
+gc()
+sapply(list.files("R", full.names = TRUE, pattern = ".R$"), source)
+sapply(list.files("src", full.names = TRUE, pattern = ".cpp$"), Rcpp::sourceCpp)
+data(meuse.grid)
+candi <- meuse.grid[, 1:2]
+schedule <- scheduleSPSANN(
+  chains = 500, stopping = 100, initial.temperature = 200, x.max = 1540, y.max = 2060, x.min = 0, y.min = 0,
+  cellsize = 40)
+free <- 10
+set.seed(1984)
+fixed <- candi[sample(1:nrow(candi), 30), ]
+set.seed(2001)
+res <- optimPPL(
+  points = list(free = free, fixed = fixed), candi = candi, schedule = schedule, plotit = TRUE)
+objSPSANN(res) - objPPL(points = res, candi = candi)
+countPPL(points = res, candi = candi)
+plot(res)
