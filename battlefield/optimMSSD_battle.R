ea3e53c2f00b1f0f27c97b0c55b815fe1af9cc6a
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-09-14 17:07:26 -0300

    tests the use of eval.grid to speed up computations and to thin an existing sample configuration

--- battlefield/optimMSSD_battle.R
+++ battlefield/optimMSSD_battle.R
@@ -4,6 +4,7 @@ gc()
 require(pedometrics)
 require(SpatialTools)
 require(sp)
+require(magrittr)
 sapply(list.files("R", full.names = TRUE, pattern = ".R$"), source)
 sapply(list.files("src", full.names = TRUE, pattern = ".cpp$"), Rcpp::sourceCpp)
 
@@ -29,14 +30,10 @@ sp::coordinates(boundary) <- c("x", "y")
 sp::gridded(boundary) <- TRUE
 boundary <- rgeos::gUnaryUnion(as(boundary, "SpatialPolygons"))
 candi <- meuse.grid[, 1:2]
-schedule <- scheduleSPSANN(
-  initial.acceptance = 0, initial.temperature = 0.01)
+schedule <- scheduleSPSANN(initial.acceptance = 0, initial.temperature = 0.01)
 set.seed(2001)
-res <- optimMSSD(
-  points = 30, candi = candi, schedule = schedule, plotit = TRUE,
-  boundary = boundary)
-objSPSANN(res)
-objMSSD(candi = candi, points = res)
+res <- optimMSSD(points = 30, candi = candi, schedule = schedule, plotit = TRUE, boundary = boundary)
+objSPSANN(res) - objMSSD(candi = candi, points = res)
 plot(res, boundary = boundary)
 
 # 2) ADD THREE POINTS TO AN ALREADY EXISTING SPATIAL SAMPLE CONFIGURATION #####################################
@@ -62,6 +59,52 @@ res <- optimMSSD(
   boundary = boundary)
 res$points[-(1:free), ]
 fixed
-objSPSANN(res)
-objMSSD(candi = candi, points = res)
+objSPSANN(res) - objMSSD(candi = candi, points = res)
 plot(res, boundary = boundary)
+
+# 3) USE A COARSER GRID (COMPARED TO CANDI) TO COMPUTE THE OBJECTIVE FUNCITON VALUE ###########################
+rm(list = ls())
+gc()
+sapply(list.files("R", full.names = TRUE, pattern = ".R$"), source)
+sapply(list.files("src", full.names = TRUE, pattern = ".cpp$"), Rcpp::sourceCpp)
+data(meuse.grid, package = "sp")
+boundary <- meuse.grid
+sp::coordinates(boundary) <- c("x", "y")
+sp::gridded(boundary) <- TRUE
+boundary <- rgeos::gUnaryUnion(as(boundary, "SpatialPolygons"))
+candi <- meuse.grid[, 1:2]
+schedule <- scheduleSPSANN(
+  chains = 100, initial.temperature = 500000, x.max = 1540, y.max = 2060, x.min = 0, y.min = 0, cellsize = 40)
+set.seed(2001)
+res_candi <- optimMSSD(
+  points = 30, candi = candi, schedule = schedule, plotit = TRUE, boundary = boundary)
+eval.grid <- sp::spsample(x = boundary, n = 1000, type = 'regular') %>% sp::coordinates()
+set.seed(2001)
+res_eval_grid <- optimMSSD(
+  points = 30, candi = candi, eval.grid = eval.grid, schedule = schedule, plotit = TRUE, boundary = boundary)
+objSPSANN(res_candi) - objMSSD(candi = candi, points = res_candi)
+objSPSANN(res_eval_grid) - objMSSD(eval.grid = eval.grid, points = res_eval_grid)
+objSPSANN(res_candi)
+objSPSANN(res_eval_grid)
+
+# 4) THIN AN EXISTING SAMPLE CONFIGURATION ####################################################################
+rm(list = ls())
+gc()
+sapply(list.files("R", full.names = TRUE, pattern = ".R$"), source)
+sapply(list.files("src", full.names = TRUE, pattern = ".cpp$"), Rcpp::sourceCpp)
+data(meuse.grid, package = "sp")
+boundary <- meuse.grid
+sp::coordinates(boundary) <- c("x", "y")
+sp::gridded(boundary) <- TRUE
+boundary <- rgeos::gUnaryUnion(as(boundary, "SpatialPolygons"))
+schedule <- scheduleSPSANN(
+  chains = 500, initial.temperature = 6000, x.max = 1540, y.max = 2060, x.min = 0, y.min = 0, cellsize = 0)
+eval.grid <- meuse.grid[, 1:2] %>% as.matrix()
+candi <- sp::spsample(x = boundary, n = 100, type = 'random') %>% sp::coordinates()
+set.seed(2001)
+res <- optimMSSD(
+  points = 50, candi = candi, eval.grid = eval.grid, schedule = schedule, plotit = TRUE, boundary = boundary)
+objSPSANN(res) - objMSSD(points = res, eval.grid = eval.grid)
+plot(boundary)
+points(res$points[, c("x", "y")], pch = 20)
+points(candi, col = 'red')
