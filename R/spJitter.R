89b507c32f1d30f6146f856e21a561f4f590e6c1
samuel-rosa <alessandrosamuelrosa@gmail.com>
2019-09-14 22:53:05 -0300

    moves documentation of spJitter to common file to be imported by other functions

--- R/spJitter.R
+++ R/spJitter.R
@@ -2,6 +2,7 @@
 #' 
 #' Randomly perturb (\sQuote{jitter}) the coordinates of spatial points.
 #' 
+#' @template spJitter_doc
 #' @param points Data frame or matrix with three columns in the following order: \code{[, "id"]} the row 
 #' indexes of \code{candi} that correspond to each point, \code{[, "x"]} the projected x-coordinates, and 
 #' \code{[, "y"]} the projected y-coordinates. Note that \code{points} must be a subset of \code{candi}.
@@ -24,49 +25,6 @@
 #' 
 #' @param verbose (Optional) Logical for printing messages about the progress of the optimization. Defaults to 
 #' `verbose = FALSE`.
-#' 
-#' @details 
-#' \subsection{Jittering methods}{
-#' There are multiple mechanism to generate a new sample configuration out of the current sample configuration.
-#' The main step consists of randomly perturbing the coordinates of a sample point, a process known as 
-#' \sQuote{jittering}. These mechanisms can be classified based on how the set of candidate locations is 
-#' defined. For example, one could use an \emph{infinite} set of candidate locations, that is, any location 
-#' in the sampling region can be selected as the new location of a jittered point. All that is needed is 
-#' a polygon indicating the boundary of the sampling region. This method is the most computationally demanding
-#' because every time a point is jittered, it is necessary to check if the point falls in sampling region.
-#' 
-#' Another approach consists of using a \emph{finite} set of candidate locations for the jittered points. A
-#' finite set of candidate locations is created by discretising the sampling region, that is, creating a fine 
-#' grid of points that serve as candidate locations for the jittered point. This is the least computationally
-#' demanding jittering method because, by definition, the jittered point will always fall in the sampling
-#' region.
-#' 
-#' Using a finite set of candidate locations has two important inconveniences. First, not all locations in 
-#' the sampling region can be selected as the new location for a jittered point. Second, when a point is 
-#' jittered, it may be that the new location already is occupied by another point. If this happens, another 
-#' location has to be iteratively sought for, say, as many times as the number of points in the sample. In 
-#' general, the more points there are in the sample, the more likely it is that the new location already is 
-#' occupied by another point. If a solution is not found in a reasonable time, the point selected to be 
-#' jittered is kept in its original location. Such a procedure clearly is suboptimal.
-#' 
-#' \pkg{spsann} uses a more elegant method which is based on using a finite set of candidate locations coupled
-#' with a form of \emph{two-stage random sampling} as implemented in 
-#' \code{[spsample](https://CRAN.R-project.org/package=spcosa)}. Because 
-#' the candidate locations are placed on a finite regular grid, they can be seen as the centre nodes of 
-#' a finite set of grid cells (or pixels of a raster image). In the first stage, one of the \dQuote{grid 
-#' cells} is selected with replacement, i.e. independently of already being occupied by another sample point. 
-#' The new location for the point chosen to be jittered is selected within that \dQuote{grid cell} by simple
-#' random sampling. This method guarantees that virtually any location in the sampling region can be selected. 
-#' It also discards the need to check if the new location already is occupied by another point, speeding up 
-#' the computations when compared to the first two approaches.
-#' }
-#' 
-#' @note 
-#' The distance between two points is computed as the Euclidean distance between them. This computation 
-#' assumes that the optimization is operating in the two-dimensional Euclidean space, i.e. the coordinates of
-#' the sample points and candidate locations should not be provided as latitude/longitude. \pkg{spsann} has no
-#' mechanism to check if the coordinates are projected: the user is responsible for making sure that this
-#' requirement is attained.
 #' 
 #' @return
 #' A matrix with the jittered projected coordinates of the points.
