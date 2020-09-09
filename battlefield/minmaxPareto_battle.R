d718e4e29dc8b6d7390081cfc31143d4b9c7fec4
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-09-09 18:10:31 -0300

    updates example

--- battlefield/minmaxPareto_battle.R
+++ battlefield/minmaxPareto_battle.R
@@ -12,29 +12,47 @@ data(meuse.grid)
 candi <- meuse.grid[, 1:2]
 covars <- meuse.grid[, c(1, 2)]
 
+# General (greedy) cooling schedule
+schedule <- scheduleSPSANN(
+  initial.acceptance = c(0.01, 0.99), chains = 100,
+  x.max = 1540, y.max = 2060, x.min = 0,
+  y.min = 0, cellsize = 40)
+
 # CORR
-schedule <- scheduleSPSANN(initial.acceptance = 0.1, chains = 1, 
-                           x.max = 1540, y.max = 2060, x.min = 0, 
-                           y.min = 0, cellsize = 40)
+schedule$initial.temperature <- 0.1
 set.seed(2001)
-osc_corr <- optimCORR(points = 10, candi = candi, covars = covars, 
-                      schedule = schedule)
+osc_corr <- optimCORR(
+  points = 10, candi = candi, covars = covars,
+  schedule = schedule)
 
 # DIST
+schedule$initial.temperature <- 0.1
 set.seed(2001)
-osc_dist <- optimDIST(points = 10, candi = candi, covars = covars,
-                      schedule = schedule)
+osc_dist <- optimDIST(
+  points = 10, candi = candi, covars = covars,
+  schedule = schedule)
 
 # PPL
+schedule$initial.temperature <- 0.1
 set.seed(2001)
-osc_ppl <- optimPPL(points = 10, candi = candi, schedule = schedule)
+osc_ppl <- optimPPL(
+  points = 10, candi = candi,
+  schedule = schedule)
 
 # MSSD
+schedule$initial.temperature <- 0.1
 set.seed(2001)
-osc_mssd <- optimMSSD(points = 10, candi = candi, schedule = schedule)
+osc_mssd <- optimMSSD(
+  points = 10, candi = candi,
+  schedule = schedule)
 
 # Pareto
-pareto <- minmaxPareto(osc = list(DIST = osc_dist, CORR = osc_corr,
-                                  PPL = osc_ppl, MSSD = osc_mssd),
-                       candi = candi, covars = covars)
-pareto
+pareto <- minmaxPareto(
+  osc = list(
+    DIST = osc_dist,
+    CORR = osc_corr,
+    PPL = osc_ppl,
+    MSSD = osc_mssd),
+  candi = candi,
+  covars = covars)
+round(pareto, 4)
