d0e7d264f8ea77f150dffb327fd96c3aca175a05
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-09-11 00:05:04 -0300

    improve computation of cutoff

--- R/prepare-jittering.R
+++ R/prepare-jittering.R
@@ -2,8 +2,8 @@
 # Please edit source code in R-autofun/prepare-jittering.R
 .prepare_jittering<-function(...){
 expression(x.min <- schedule$x.min, y.min <- schedule$y.min, 
-    cellsize <- schedule$cellsize, x.max <- ifelse(test = is.null(schedule$x.max), yes = range(candi[, 'x']) / 2, no = schedule$x.max), 
-    y.max <- ifelse(test = is.null(schedule$y.max), yes = range(candi[, 'y']) / 2, no = schedule$y.max), 
+    cellsize <- schedule$cellsize, x.max <- ifelse(test = is.null(schedule$x.max), yes = diff(range(candi[, 'x'])) / 2, no = schedule$x.max), 
+    y.max <- ifelse(test = is.null(schedule$y.max), yes = diff(range(candi[, 'y'])) / 2, no = schedule$y.max), 
     x_max0 <- x.max, y_max0 <- y.max, if (all(cellsize == 0)) {
       x_min0 <- SpatialTools::dist1(as.matrix(candi[, "x"]))
       y_min0 <- SpatialTools::dist1(as.matrix(candi[, "y"]))
